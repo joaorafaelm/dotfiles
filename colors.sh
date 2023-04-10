a4f7f2eab678f96b10b1dfb508429bc64879102a
João Rafael <joaoraf@me.com>
2023-04-10 20:54:19 -0300

    Refactor color display in colors.sh script.

--- colors.sh
+++ colors.sh
@@ -6,10 +6,10 @@
 # To Public License, Version 2, as published by Sam Hocevar. See
 # http://sam.zoy.org/wtfpl/COPYING for more details.
 
-for color in {0..255} ; do #Colors
+for color in {0..255} ; do
     #Display the color
     printf "\e[38;5;%sm  %3s  \e[0m" $color $color
-    printf "\e[48;5;%sm  %3s  \e[0m" $color
+    printf "\e[48;5;%sm%3s\e[0m" $color
     #Display 6 colors per lines
     if [ $((($color + 1) % 6)) == 4 ] ; then
         echo #New line
