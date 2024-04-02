6a02c3c96cdaaf28ab03734b364f775ad6a5333a
João Rafael <joaoraf@me.com>
2024-04-02 20:17:31 -0300

    feat: change cell representation from square to asterisk in life.py

--- games/life.py
+++ games/life.py
@@ -7,7 +7,7 @@ N = range(GRID_SIZE)
 
 while 1:
     for i in N:
-        print("".join(" ⬛"[i * GRID_SIZE + j in P] for j in N))
+        print("".join(" *"[i * GRID_SIZE + j in P] for j in N))
     time.sleep(0.1)
     Q = [(p + d) % (GRID_SIZE**2) for d in (-GRID_SIZE-1, -GRID_SIZE, -GRID_SIZE+1, -1, 1, GRID_SIZE-1, GRID_SIZE, GRID_SIZE+1) for p in P]
     P = set(p for p in Q if 2 - ({p} < P) < Q.count(p) < 4)
