3fd4b78e36f68ff609ec81e0f48622291da1b1dd
João Rafael <joaoraf@me.com>
2024-04-02 20:09:08 -0300

    feat: adjust y offset in alacritty.toml and add new life.py game

---
+++ games/life.py
@@ -0,0 +1,13 @@
+import time
+from random import random
+
+GRID_SIZE = 50
+P = {i for i in range(GRID_SIZE**2) if random() < 0.1}
+N = list(range(GRID_SIZE))
+
+while 1:
+    for i in N:
+        print("".join(" ⬛"[i * GRID_SIZE + j in P] for j in N))
+    time.sleep(0.2)
+    Q = [(p + d) % (GRID_SIZE**2) for d in (-GRID_SIZE-1, -GRID_SIZE, -GRID_SIZE+1, -1, 1, GRID_SIZE-1, GRID_SIZE, GRID_SIZE+1) for p in P]
+    P = set(p for p in Q if 2 - ({p} < P) < Q.count(p) < 4)
