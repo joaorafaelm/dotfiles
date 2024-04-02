6bae84315636fe8b6a7e68f486e0976bd6d1daeb
João Rafael <joaoraf@me.com>
2024-04-02 20:10:36 -0300

    feat(games): optimize code and reduce sleep duration in life.py

--- games/life.py
+++ games/life.py
@@ -3,11 +3,11 @@ from random import random
 
 GRID_SIZE = 50
 P = {i for i in range(GRID_SIZE**2) if random() < 0.1}
-N = list(range(GRID_SIZE))
+N = range(GRID_SIZE)
 
 while 1:
     for i in N:
         print("".join(" ⬛"[i * GRID_SIZE + j in P] for j in N))
-    time.sleep(0.2)
+    time.sleep(0.1)
     Q = [(p + d) % (GRID_SIZE**2) for d in (-GRID_SIZE-1, -GRID_SIZE, -GRID_SIZE+1, -1, 1, GRID_SIZE-1, GRID_SIZE, GRID_SIZE+1) for p in P]
     P = set(p for p in Q if 2 - ({p} < P) < Q.count(p) < 4)
