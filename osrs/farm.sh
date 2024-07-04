0881b78a557719d733959fa3353ea7497e3bb7aa
João Rafael <joaoraf@me.com>
2024-07-04 18:18:02 -0300

    [master]

---
+++ osrs/farm.sh
@@ -0,0 +1,19 @@
+counter=0
+while true; do
+    for i in {1..150}
+    do
+        open -a RuneLite
+        # automator -v osrs/cheap_ha.workflow >> /dev/null
+        # automator -v osrs/string_bow.workflow >> /dev/null && sleep 20
+        # automator -v ./bow.workflow >> /dev/null && sleep 47
+        automator -v ./cook.workflow >> /dev/null && sleep 65
+        printf "\r%d" $i
+        counter=$((counter+1))
+        if [ $counter -eq 280 ]; then
+            exit 0
+        fi
+    done
+
+    echo "\nlogging out and logging in...\n"
+    automator ./logout_login_fletching.workflow >> /dev/null
+done
