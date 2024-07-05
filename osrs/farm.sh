8fa4628e980cfa804d2dd8ff01548605b6b5a266
João Rafael <joaoraf@me.com>
2024-07-05 16:57:28 -0300

    refactor: remove verbose flag from gitconfig and update plugin commit hashes

--- osrs/farm.sh
+++ osrs/farm.sh
@@ -2,11 +2,11 @@ counter=0
 while true; do
     for i in {1..150}
     do
-        open -a RuneLite
+        open -a RuneLite && sleep 1
         # automator -v osrs/cheap_ha.workflow >> /dev/null
         # automator -v osrs/string_bow.workflow >> /dev/null && sleep 20
         # automator -v ./bow.workflow >> /dev/null && sleep 47
-        automator -v ./cook.workflow >> /dev/null && sleep 65
+        automator -v ./cook.workflow >> /dev/null && sleep 64
         printf "\r%d" $i
         counter=$((counter+1))
         if [ $counter -eq 280 ]; then
